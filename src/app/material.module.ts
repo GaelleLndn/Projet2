035326a4dfe1bbbaabee59e637360cb348d21639
Gaëlle <gaellelondon@hotmail.com>
2018-08-25 15:46:38 +0200

    merge

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,20 +2,6 @@ import { NgModule } from '@angular/core';
 import { MatTabsModule, MatInputModule, MatChipsModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, MatProgressSpinnerModule, MatDatepickerModule, MatNativeDateModule, MatSelectModule, MatDialogModule} from '@angular/material'
 
 @NgModule({
-  imports: [
-    MatChipsModule,
-    MatInputModule, 
-    MatCardModule,
-    MatButtonModule, 
-    MatToolbarModule,
-    MatExpansionModule, 
-    MatProgressSpinnerModule,
-    MatDatepickerModule,
-    MatNativeDateModule, 
-    MatSelectModule,
-    MatTabsModule, 
-    MatDialogModule
-  ],
   exports:[
     MatChipsModule,
     MatInputModule, 
